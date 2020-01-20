8a18de08fd26c298ed8e88f182ee676a6ae6ec44
tahakkani <38796045+tahakkani@users.noreply.github.com>
2020-01-20 11:15:58 -0800

    Add exclamation marks to second out

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("HelloWorld!");
-        System.out.println("another one");
+        System.out.println("another one!!!");
     }
 }
 
